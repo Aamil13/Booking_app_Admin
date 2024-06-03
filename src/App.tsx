84877b0547ace7d94958a9188b13e0bf91efd799
Aamil13 <aamil.shafi13@gmail.com>
2024-06-03 20:49:03 +0530

    initialized the project with vite-typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Button className="bg-red-500 text-white">first</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+import './index.css';
+import { StyleProvider } from '@ant-design/cssinjs';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <StyleProvider layer>
+      <App />
+    </StyleProvider>
+  </React.StrictMode>
+);
